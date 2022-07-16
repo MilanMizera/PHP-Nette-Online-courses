9b4d7165496bfe3a32ef56780bbad1063d953ca8
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-07-16 20:11:36 +0200

    Update HomepagePresenter.php

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,8 +5,23 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+}
+
+ function renderDefault(): void
+{
+	$this->template->posts = $this->database
+		->table('posts')
+		->order('created_at DESC')
+		->limit(5);
 }
