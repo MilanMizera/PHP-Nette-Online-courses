806790d0dd0d9836ab14941dd0e6dffdd3b0ce8a
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-01 23:39:13 +0200

    login/register page

---
+++ app/Presenters/RegisterPresenter.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+
+final class RegisterPresenter extends Nette\Application\UI\Presenter {
+
+    protected function createComponentRegisterForm(): Form {
+
+	$form = new Form; // means Nette\Application\UI\Form
+
+
+	$form->addEmail('email', 'E-mail:');
+    
+	$form->addPassword('password', 'Heslo:')
+		->setRequired();
+
+        $form->addCheckbox('agree', 'Souhlasím se zpracováním osobních údajů')
+        ->setRequired('Je potřeba souhlasit s osobníma údajema');
+    
+	$form->addSubmit('send', 'Přihlásit');
+    
+	return $form;
+}
+}
--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -17,6 +17,7 @@ final class RouterFactory
 		$router = new RouteList;
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
 		$router->addRoute('prihlaseni/','Sign:in');
+		$router->addRoute('registrace/','Register:out');
 		return $router;
 	}
 }
