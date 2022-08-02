4cfe7f213b5923b0e426ceaa306c565fa2f53f6a
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-02 16:47:20 +0200

    register page

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -17,7 +17,7 @@ final class RouterFactory
 		$router = new RouteList;
 		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
 		$router->addRoute('prihlaseni/','Sign:in');
-		$router->addRoute('registrace/','Register:out');
+		$router->addRoute('registrace/','Register:register');
 		return $router;
 	}
 }
