46aa52e912b52a6b6dea57de798e4d89f6de67f2
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-08-24 15:24:32 +0200

    Update RegisterPresenter.php

--- app/Presenters/RegisterPresenter.php
+++ app/Presenters/RegisterPresenter.php
@@ -10,56 +10,32 @@ use Nette\Application\UI\Form;
 
 final class RegisterPresenter extends Nette\Application\UI\Presenter {
 
-    //Připojení do databáze
-    private Nette\Database\Explorer $database;
-
-	public function __construct(Nette\Database\Explorer $database)
-	{
-		$this->database = $database;
-	}
-
     //vytvoření registračního formulář
     protected function createComponentRegisterForm(): Form {
 
-	$form = new Form; // means Nette\Application\UI\Form
+        $form = new Form; // means Nette\Application\UI\Form
 
-    $form->addText('name', 'Jméno:')
-    ->setRequired('Prosím, vyplńte Vaše jméno');
+        $form->addText('name', 'Jméno:')
+            ->setRequired('Prosím, vyplńte Vaše jméno');
 
-    $form->addText('surname', 'Příjmení:')
-    ->setRequired('Prosím, vyplńte Vaše příjmení');
+        $form->addText('surname', 'Příjmení:')
+            ->setRequired('Prosím, vyplńte Vaše příjmení');
 
-	$form->addEmail('email', 'E-mail:')
-    ->setRequired('Prosím, vyplńte Vaší emailovou adresu');
+        $form->addEmail('email', 'E-mail:')
+            ->setRequired('Prosím, vyplńte Vaší emailovou adresu');
 
-	$form->addPassword('password', 'Heslo:')
-		->setRequired('Prosím, vyplńte Vaše heslo');
+        $form->addPassword('password', 'Heslo:')
+            ->setRequired('Prosím, vyplńte Vaše heslo');
 
         $form->addCheckbox('agree', 'Souhlasím se zpracováním osobních údajů')
-        ->setRequired('Je potřeba souhlasit s osobníma údajema')
-        ->setHtmlAttribute('class','register_check');
-    
-	$form->addSubmit('send', 'Registrovat');
-    
-    $form->onSuccess[] = [$this, 'registerFormSucceeded'];
+            ->setRequired('Je potřeba souhlasit s osobníma údajema')
+            ->setHtmlAttribute('class','register_check');
+        
+        $form->addSubmit('send', 'Registrovat');
+        
+        $form->onSuccess[] = [$this, 'registerFormSucceeded'];
 
-	return $form;
+        return $form;
     }
-
-    //vložení dat do db
-    public function registerFormSucceeded(\stdClass $data): void {
-	
-	$this->database->table('registrace')->insert([
-
-		'jmeno' => $data->name,
-		'prijmeni' => $data->surname,
-		'email' => $data->email,
-        'heslo' => $data->password,
-	]);
-
-	$this->flashMessage('Úspěšně jste se registroval/a', 'success');
-	$this->redirect('this');
-}
-
 
 }
